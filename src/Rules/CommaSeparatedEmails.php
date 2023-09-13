daf32b800532b350045158c2b5fad83a60dd6c5e
Irineu Junior <irineu@fmd.ag>
2023-09-13 15:54:07 -0300

    feat(rules) create rule to cc and bcc

---
+++ src/Rules/CommaSeparatedEmails.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Agenciafmd\Support\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+
+class CommaSeparatedEmails implements Rule
+{
+    public function passes(mixed $attribute, mixed $value): bool
+    {
+        $value = str_replace([' ', ';'], ',', $value);
+        $value = array_map('trim', explode(',', $value));
+        $rules = [
+            'email' => [
+                'required',
+                'email:rfc,dns',
+            ],
+        ];
+        foreach ($value as $email) {
+            $data = [
+                'email' => $email,
+            ];
+            $validator = Validator::make($data, $rules);
+            if ($validator->fails()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function message(): string
+    {
+        return __('validation.email');
+    }
+}
