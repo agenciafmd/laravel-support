6309d6806494209475365cdb187e2234b110cee0
Irineu Junior <irineu@fmd.ag>
2024-11-07 16:40:50 -0300

    feat(livewire) adiciona synth que converte string vazia em null

--- src/Providers/SupportServiceProvider.php
+++ src/Providers/SupportServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace Agenciafmd\Support\Providers;
 
+use Agenciafmd\Support\Livewire\Synthesizers\StringSynth;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class SupportServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         $this->providers();
+        $this->loadLivewireSynth();
     }
 
     public function register(): void
@@ -22,5 +25,10 @@ class SupportServiceProvider extends ServiceProvider
         $this->app->register(EloquentServiceProvider::class);
         $this->app->register(RequestServiceProvider::class);
         $this->app->register(StrServiceProvider::class);
+    }
+
+    private function loadLivewireSynth(): void
+    {
+        Livewire::propertySynthesizer(StringSynth::class);
     }
 }
--- src/Rules/CommaSeparatedEmails.php
+++ src/Rules/CommaSeparatedEmails.php
@@ -5,6 +5,8 @@ namespace Agenciafmd\Support\Rules;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
 
+/* TODO: refac https://laravel.com/docs/11.x/validation#using-rule-objects*/
+
 class CommaSeparatedEmails implements Rule
 {
     public function passes(mixed $attribute, mixed $value): bool
