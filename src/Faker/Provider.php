685f3d675ed2fe68f546ec58247ec563e5f6e3c3
Irineu Junior <irineu@fmd.ag>
2026-01-22 16:45:13 -0300

    refactor(provider): melhorar métodos de geração de dados fictícios
    Atualiza a classe Provider para utilizar expressões match em vez de
    switch, simplificando a lógica de seleção de URIs do YouTube. Adiciona
    novos métodos para gerar imagens locais, tags e textos HTML, além de
    implementar validações para os parâmetros de entrada.

--- src/Faker/Provider.php
+++ src/Faker/Provider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Agenciafmd\Support\Faker;
 
 use Faker\Provider\Base;
+use InvalidArgumentException;
 
 final class Provider extends Base
 {
@@ -54,26 +55,83 @@ final class Provider extends Base
 
     public function youtubeRandomUri(): string
     {
-        switch (self::numberBetween(1, 3)) {
-            case 1:
-                return $this->youtubeUri();
+        return match (self::numberBetween(1, 3)) {
+            1 => $this->youtubeShortUri(),
+            2 => $this->youtubeEmbedUri(),
+            default => $this->youtubeUri(),
+        };
+    }
 
-                break;
-
-            case 2:
-                return $this->youtubeShortUri();
-
-                break;
-
-            case 3:
-                return $this->youtubeEmbedUri();
-
-                break;
-
-            default:
-                return $this->youtubeUri();
-
-                break;
+    public function localImage($ratio = '16x9', $sourceDir = null): string
+    {
+        $ratio = str_replace(':', 'x', $ratio);
+        if (! in_array($ratio, ['1x1', '4x3', '16x9', '21x9'], true)) {
+            throw new InvalidArgumentException(sprintf('Invalid ratio "%s"', $ratio));
         }
+
+        if (! $sourceDir) {
+            $sourceDir = __DIR__ . "/../../resources/faker/images/{$ratio}/";
+        }
+
+        return fake()->file($sourceDir);
+    }
+
+    public function tags($max = 3, $allowed = []): array
+    {
+        if (! count($allowed)) {
+            $allowed = [
+                'Economia',
+                'Financiamento',
+                'Projetos',
+                'Sustentabilidade',
+                'Tendências',
+            ];
+        }
+
+        return fake()->randomElements($allowed, fake()->numberBetween(1, $max));
+    }
+
+    public function htmlParagraph($nbSentences = 3): string
+    {
+        return '<p>' . fake()->paragraph($nbSentences) . '</p>';
+    }
+
+    public function htmlParagraphs($nbParagraphs = 10): string
+    {
+        return collect(range(1, $nbParagraphs))
+            ->map(fn () => fake()->paragraph(10))
+            ->map(fn ($paragraph) => $this->saltTags($paragraph))
+            ->map(fn ($paragraph) => "<p>{$paragraph}</p>")
+            ->implode('');
+    }
+
+    public function htmlText(): string
+    {
+        $blocks[] = $this->htmlParagraphs(fake()->numberBetween(3, 6));
+        $blocks[] = sprintf("<p><img src=\"%s\" width=\"800\" style=\"width\: 800; height\: 600;\" height=\"600\" data-id=\"%s\"></p>", "https://picsum.photos/800/600?random=" . random_int(10000, 15999), fake()->uuid());
+        $blocks[] = sprintf('<blockquote>%s</blockquote>', $this->htmlParagraphs(fake()->numberBetween(1, 3)));
+        $blocks[] = sprintf('<details><summary>%s</summary><div data-type="detailsContent">%s</div></details>', fake()->sentence(3), $this->htmlParagraphs(fake()->numberBetween(1, 3)));
+
+        return implode('', $blocks);
+    }
+
+    private function saltTags($text, $nbTags = 3): string
+    {
+        $tags = [
+            '<strong>%s</strong>',
+            '<em>%s</em>',
+            '<u>%s</u>',
+            '<s>%s</s>',
+            '<a href="https://fmd.ag">%s</a>',
+        ];
+        shuffle($tags);
+        $words = explode(' ', $text);
+        shuffle($words);
+        $taggedWords = [];
+        for ($i = 0; $i < $nbTags; $i++) {
+            $taggedWords[$words[$i]] = sprintf($tags[$i], $words[$i]);
+        }
+
+        return str_replace(array_keys($taggedWords), array_values($taggedWords), $text);
     }
 }
